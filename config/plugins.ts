3988d0322f4dd30d1373da6a00fb91c4211b8d9a
Fidrick Benjamin <fidrick1@gmail.com>
2025-04-03 23:46:30 -0400

    env fix

--- config/plugins.ts
+++ config/plugins.ts
@@ -8,9 +8,9 @@ export default ({ env }) => ({
     config: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('dwxoqsw0v'),
-        api_key: env('918448345143175'),
-        api_secret: env('3pwPSpZo0RF5Wd2G7elKbix0Zok'),
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
     },
   },
