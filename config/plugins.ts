c45c2006766e2f4b054ef5e0290afc024f1ad3c4
Fidrick Benjamin <fidrick1@gmail.com>
2025-04-03 23:40:20 -0400

    cloudinary integration

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,17 @@
 export default ({ env }) => ({
-    'users-permissions': {
-      config: {
-        jwtSecret: env('JWT_SECRET'),
+  'users-permissions': {
+    config: {
+      jwtSecret: env('JWT_SECRET'),
+    },
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('dwxoqsw0v'),
+        api_key: env('918448345143175'),
+        api_secret: env('3pwPSpZo0RF5Wd2G7elKbix0Zok'),
       },
     },
-  });
+  },
+});
